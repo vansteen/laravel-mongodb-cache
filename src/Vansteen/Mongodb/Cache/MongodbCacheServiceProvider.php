5764a1dc831e5ca85b01c9f5c4c3deb0eb73aa6d
Ibraheem Z. Abu-Kaff <ibra.abukaff@tajawal.com>
2016-06-03 23:04:11 +0400

    Major fixes on forget, cache contact , and PHP7 mongodate issue

--- src/Vansteen/Mongodb/Cache/MongodbCacheServiceProvider.php
+++ src/Vansteen/Mongodb/Cache/MongodbCacheServiceProvider.php
@@ -2,23 +2,22 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class MongodbCacheServiceProvider extends ServiceProvider {
+class MongodbCacheServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-    $this->app->resolving('cache', function($cache)
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
     {
-        $cache->extend('mongodb', function($app)
-        {
-            $manager = new MongodbCacheManager($app);
+        $this->app->resolving('cache', function ($cache) {
+            $cache->extend('mongodb', function ($app) {
+                $manager = new MongodbCacheManager($app);
 
-            return $manager->driver('mongodb');
+                return $manager->driver('mongodb');
+            });
         });
-    });
-	}
+    }
 }
--- src/Vansteen/Mongodb/Cache/MongodbStore.php
+++ src/Vansteen/Mongodb/Cache/MongodbStore.php
@@ -1,229 +1,254 @@
-<?php namespace Vansteen\Mongodb\Cache;
+<?php
+namespace Vansteen\Mongodb\Cache;
 
 use Jenssegers\Mongodb\Connection;
-use Illuminate\Cache\StoreInterface;
+use Illuminate\Contracts\Cache\Store as StoreInterface;
 use Illuminate\Encryption\Encrypter;
+use MongoDB\BSON\UTCDatetime;
 
-class MongodbStore implements StoreInterface {
+class MongodbStore implements StoreInterface
+{
 
-	/**
-	 * The database connection instance.
-	 *
-	 * @var \Jenssegers\Mongodb\Connection
-	 */
-	protected $connection;
+    public function putMany(array $values, $minutes)
+    {
 
-	/**
-	 * The encrypter instance.
-	 *
-	 * @var \Illuminate\Encryption\Encrypter
-	 */
-	protected $encrypter;
+    }
 
-	/**
-	 * The name of the cache collection.
-	 *
-	 * @var string
-	 */
-	protected $collection_name;
+    public function many(array $keys)
+    {
 
-	/**
-	 * A string that should be prepended to keys.
-	 *
-	 * @var string
-	 */
-	protected $prefix;
+    }
 
-	/**
-	 * Create a new database store.
-	 *
-	 * @param  Jenssegers\Mongodb\Connection  $connection
-	 * @param  \Illuminate\Encryption\Encrypter  $encrypter
-	 * @param  string  $table
-	 * @param  string  $prefix
-	 * @return void
-	 */
-	public function __construct(Connection $connection, Encrypter $encrypter, $collection_name, $prefix = '')
-	{
-		$this->connection = $connection;
-		$this->encrypter = $encrypter;
-		$this->collection_name = $collection_name;
-		$this->prefix = $prefix;
-	}
+    /**
+     * The database connection instance.
+     *
+     * @var \Jenssegers\Mongodb\Connection
+     */
+    protected $connection;
 
-	/**
-	 * Retrieve an item from the cache by key.
-	 *
-	 * @param  string  $key
-	 * @return mixed
-	 */
-	public function get($key)
-	{
-		$key = $this->prefix . $key;
+    /**
+     * The encrypter instance.
+     *
+     * @var \Illuminate\Encryption\Encrypter
+     */
+    protected $encrypter;
 
-		$cache = $this->getCacheCollection()->where('key', $key)->first();
+    /**
+     * The name of the cache collection.
+     *
+     * @var string
+     */
+    protected $collection_name;
 
-		// If we have a cache record we will check the expiration time against current
-		// time on the system and see if the record has expired. If it has, we will
-		// remove the records from the database collection so it isn't returned again.
-		if ( ! is_null($cache))
-		{
-			if (time() >= $cache['expiration']->sec)
-			{
-				return $this->forget($key);
-			}
+    /**
+     * A string that should be prepended to keys.
+     *
+     * @var string
+     */
+    protected $prefix;
 
-			return $this->encrypter->decrypt($cache['value']);
-		}
-	}
+    /**
+     * Create a new database store.
+     *
+     * @param  Jenssegers\Mongodb\Connection    $connection
+     * @param  \Illuminate\Encryption\Encrypter $encrypter
+     * @param  string                           $table
+     * @param  string                           $prefix
+     *
+     * @return void
+     */
+    public function __construct(Connection $connection, Encrypter $encrypter, $collection_name, $prefix = '')
+    {
+        $this->connection      = $connection;
+        $this->encrypter       = $encrypter;
+        $this->collection_name = $collection_name;
+        $this->prefix          = $prefix;
+    }
 
-	/**
-	 * Store an item in the cache for a given number of minutes.
-	 *
-	 * @param  string  $key
-	 * @param  mixed   $value
-	 * @param  int     $minutes
-	 * @return void
-	 */
-	public function put($key, $value, $minutes)
-	{
-		$key = $this->prefix . $key;
+    /**
+     * Retrieve an item from the cache by key.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $key = $this->prefix . $key;
 
-		// All of the cached values in the database are encrypted in case this is used
-		// as a session data store by the consumer. We'll also calculate the expire
-		// time and place that on the table so we will check it on our retrieval.
-		$value = $this->encrypter->encrypt($value);
+        $cache = $this->getCacheCollection()->where('key', $key)->first();
 
-		$expiration = new \MongoDate($this->getTime() + ($minutes * 60));
+        // If we have a cache record we will check the expiration time against current
+        // time on the system and see if the record has expired. If it has, we will
+        // remove the records from the database collection so it isn't returned again.
+        if (!is_null($cache)) {
+            if (class_exists('MongoDate')) {
+                if (time() >= $cache['expiration']->sec) {
+                    return $this->forget($key);
+                }
+            } else {
+                if (time() * 1000 >= intval((string)($cache['expiration']))) {
+                    return $this->forget($key);
+                }
+            }
 
-		$data = array('expiration' => $expiration, 'key' => $key, 'value' => $value);
 
-		$item = $this->getCacheCollection()->where('key', $key)->first();
+            return $this->encrypter->decrypt($cache['value']);
+        }
+    }
 
-		if(is_null($item))
-		{
-			$this->getCacheCollection()->insert($data);
-		}
-		else
-		{
-			$this->getCacheCollection()->where('key', $key)->update($data);
-		}
-	}
+    /**
+     * Store an item in the cache for a given number of minutes.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     * @param  int    $minutes
+     *
+     * @return void
+     */
+    public function put($key, $value, $minutes)
+    {
+        $key = $this->prefix . $key;
 
-	/**
-	 * Increment the value of an item in the cache.
-	 *
-	 * @param  string  $key
-	 * @param  mixed   $value
-	 * @return void
-	 *
-	 * @throws \LogicException
-	 */
-	public function increment($key, $value = 1)
-	{
-		throw new \LogicException("Increment operations not supported by this driver.");
-	}
+        // All of the cached values in the database are encrypted in case this is used
+        // as a session data store by the consumer. We'll also calculate the expire
+        // time and place that on the table so we will check it on our retrieval.
+        $value = $this->encrypter->encrypt($value);
 
-	/**
-	 * Increment the value of an item in the cache.
-	 *
-	 * @param  string  $key
-	 * @param  mixed   $value
-	 * @return void
-	 *
-	 * @throws \LogicException
-	 */
-	public function decrement($key, $value = 1)
-	{
-		throw new \LogicException("Increment operations not supported by this driver.");
-	}
+        if (!class_exists('MongoDate')) {
+            $expiration = new UTCDatetime((strtotime(date(DATE_ISO8601, time())) + ($minutes * 60)) * 1000);
+        } else {
+            $expiration = new \MongoDate($this->getTime() + ($minutes * 60));
+        }
 
-	/**
-	 * Get the current system time.
-	 *
-	 * @return int
-	 */
-	protected function getTime()
-	{
-		return time();
-	}
+        $data = ['expiration' => $expiration, 'key' => $key, 'value' => $value];
 
-	/**
-	 * Store an item in the cache indefinitely.
-	 *
-	 * @param  string  $key
-	 * @param  mixed   $value
-	 * @return void
-	 */
-	public function forever($key, $value)
-	{
-		return $this->put($key, $value, 5256000);
-	}
+        $item = $this->getCacheCollection()->where('key', $key)->first();
 
-	/**
-	 * Remove an item from the cache.
-	 *
-	 * @param  string  $key
-	 * @return void
-	 */
-	public function forget($key)
-	{
-		$item = $this->getCacheCollection()->where('key', $key)->first();
+        if (is_null($item)) {
+            $this->getCacheCollection()->insert($data);
+        } else {
+            $this->getCacheCollection()->where('key', $key)->update($data);
+        }
+    }
 
-		if(!is_null($item))
-		{
-			$this->getCacheCollection()->where('key', $key)->delete();
-		}
-	}
+    /**
+     * Increment the value of an item in the cache.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     *
+     * @return void
+     *
+     * @throws \LogicException
+     */
+    public function increment($key, $value = 1)
+    {
+        throw new \LogicException("Increment operations not supported by this driver.");
+    }
 
-	/**
-	 * Remove all items from the cache.
-	 *
-	 * @return void
-	 */
-	public function flush()
-	{
-		$mongo_collection = $this->connection->getCollection($this->collection_name);
-		$mongo_collection->drop();
-	}
+    /**
+     * Increment the value of an item in the cache.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     *
+     * @return void
+     *
+     * @throws \LogicException
+     */
+    public function decrement($key, $value = 1)
+    {
+        throw new \LogicException("Increment operations not supported by this driver.");
+    }
 
-	/**
-	 * Get the underlying database connection.
-	 *
-	 * @return \Jenssegers\Mongodb\Connection
-	 */
-	public function getConnection()
-	{
-		return $this->connection->getMongoClient();
-	}
+    /**
+     * Get the current system time.
+     *
+     * @return int
+     */
+    protected function getTime()
+    {
+        return time();
+    }
 
-	/**
-	 * Get the encrypter instance.
-	 *
-	 * @return \Illuminate\Encryption\Encrypter
-	 */
-	public function getEncrypter()
-	{
-		return $this->encrypter;
-	}
+    /**
+     * Store an item in the cache indefinitely.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     *
+     * @return void
+     */
+    public function forever($key, $value)
+    {
+        return $this->put($key, $value, 5256000);
+    }
 
-	/**
-	 * Get the cache key prefix.
-	 *
-	 * @return string
-	 */
-	public function getPrefix()
-	{
-		return $this->prefix;
-	}
+    /**
+     * Remove an item from the cache.
+     *
+     * @param  string $key
+     *
+     * @return void
+     */
+    public function forget($key)
+    {
+        $key  = $this->prefix . $key;
+        $item = $this->getCacheCollection()->where('key', $key)->first();
+        if (!is_null($item)) {
+            $this->getCacheCollection()->where('key', $key)->delete();
+        }
+    }
 
-	/**
-	 * Get the collection.
-	 *
-	 * @return QueryBuilder
-	 */
-	protected function getCacheCollection()
-	{
-		return $this->connection->collection($this->collection_name);
-	}
+    /**
+     * Remove all items from the cache.
+     *
+     * @return void
+     */
+    public function flush()
+    {
+        $mongo_collection = $this->connection->getCollection($this->collection_name);
+        $mongo_collection->drop();
+    }
+
+    /**
+     * Get the underlying database connection.
+     *
+     * @return \Jenssegers\Mongodb\Connection
+     */
+    public function getConnection()
+    {
+        return $this->connection->getMongoClient();
+    }
+
+    /**
+     * Get the encrypter instance.
+     *
+     * @return \Illuminate\Encryption\Encrypter
+     */
+    public function getEncrypter()
+    {
+        return $this->encrypter;
+    }
+
+    /**
+     * Get the cache key prefix.
+     *
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * Get the collection.
+     *
+     * @return QueryBuilder
+     */
+    protected function getCacheCollection()
+    {
+        return $this->connection->collection($this->collection_name);
+    }
 }
